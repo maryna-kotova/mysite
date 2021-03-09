0b4f24dde9e46cf392b1d9262e109659918980a9
Kotova Maryna <marynakotova256@gmail.com>
2021-03-09 08:24:33 +0200

    last

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/ 
 
 Route::get('/', [MainController::class, 'index']);
 Route::get('/profile', [ProfileController::class, 'user'])->middleware('auth');
